65729b98751bacfdbd018de6b378ed9f21388b71
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-05 19:41:30 +0000

    Use meta-config and commons-configuration to get location of ORDS server config, rather than CommonVars class

--- src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/ServerConfigurationServiceImpl.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/ServerConfigurationServiceImpl.java
@@ -8,29 +8,48 @@ import org.apache.commons.configuration.XMLConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.ox.it.ords.api.statistics.configuration.CommonVars;
 import uk.ac.ox.it.ords.api.statistics.services.ServerConfigurationService;
 
 public class ServerConfigurationServiceImpl implements ServerConfigurationService {
 
 	Logger log = LoggerFactory.getLogger(ServerConfigurationServiceImpl.class);
+	
+	public static final String DEFAULT_SERVER_CONFIG_LOCATION = "/etc/ordsConfig/serverConfig.xml";
+
 
 	@Override
 	public List<String> getServers() throws Exception {
+		
+		String serverConfigurationLocation = DEFAULT_SERVER_CONFIG_LOCATION;
+
+		try {
+			//
+			// Load the meta-configuration file
+			//
+			XMLConfiguration config = new XMLConfiguration("config.xml");
+			serverConfigurationLocation = config.getString("serverConfigurationLocation");
+			if (serverConfigurationLocation == null){
+				log.warn("No server configuration location set; using defaults");
+				serverConfigurationLocation = DEFAULT_SERVER_CONFIG_LOCATION;
+			}
+		} catch (Exception e) {
+			log.warn("No server configuration location set; using defaults");
+			serverConfigurationLocation = DEFAULT_SERVER_CONFIG_LOCATION;
+		}
 		
 		try {
 			//
 			// Load the configuration file
 			//
-			XMLConfiguration config = new XMLConfiguration(CommonVars.serverConfig);
-			
+			XMLConfiguration serverConfig = new XMLConfiguration(serverConfigurationLocation);
+
 			//
 			// Read the server list
 			//
-			String[] servers = config.getStringArray("serverList.server[@name]");
+			String[] servers = serverConfig.getStringArray("serverList.server[@name]");
 			return Arrays.asList(servers);
 		} catch (Exception e) {
-			log.error("Unable to find server configuration file in " + CommonVars.serverConfig);
+			log.error("Unable to find server configuration file in " + serverConfigurationLocation);
 			throw new FileNotFoundException();
 		}
 		
