d8b54fff87191b3ca19495fd7f2dccd33bf6e146
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-05 19:40:49 +0000

    Use Commons-Configuration to read in the dbProperties rather than a custom file method

--- src/main/java/uk/ac/ox/it/ords/api/statistics/configuration/AuthenticationDetails.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/configuration/AuthenticationDetails.java
@@ -1,68 +1,81 @@
 package uk.ac.ox.it.ords.api.statistics.configuration;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AuthenticationDetails {
-	
+
 	private static Logger log = LoggerFactory.getLogger(AuthenticationDetails.class);
-	
+
+	public static final String DEFAULT_DATABASE_CONFIG_LOCATION = "/etc/ordsConfig/db.properties";
+
 	private String ordsUser, ordsPassword;
 	private String datasetViewer, datasetViewerPassword;
 	private String rootDbUser, rootDbPassword;
 	private String ordsOdbcUserMasterPassword;
 
 	public AuthenticationDetails() {
+
+		String dbConfigurationLocation = DEFAULT_DATABASE_CONFIG_LOCATION;
 		try {
-			Properties prop = readProperties(CommonVars.mainPropertiesFile);
-			
+
+			//
+			// Load the meta-configuration file
+			//
+			XMLConfiguration config = new XMLConfiguration("config.xml");
+			dbConfigurationLocation = config.getString("databasePropertiesLocation");
+			if (dbConfigurationLocation == null){
+				log.warn("No server configuration location set; using defaults");
+				dbConfigurationLocation = DEFAULT_DATABASE_CONFIG_LOCATION;
+			}
+		} catch (Exception e) {
+			log.warn("No server configuration location set; using defaults");
+			dbConfigurationLocation = DEFAULT_DATABASE_CONFIG_LOCATION;
+		}
+		
+		try {
+
+			//
+			// Load the Database Properties
+			//
+			PropertiesConfiguration prop = new PropertiesConfiguration(dbConfigurationLocation);
+
 			/**
 			 * The user that can connect to and read/write from/to the ords database. Used for hibernate.
 			 */
-			ordsUser = prop.getProperty("user");
-			ordsPassword = prop.getProperty("password");
-			
+			ordsUser = prop.getString("user");
+			ordsPassword = prop.getString("password");
+
 			/**
 			 * A read only user used to view public datasets
 			 */
-			datasetViewer = prop.getProperty("datasetViewer");
-			datasetViewerPassword = prop.getProperty("datasetViewerPassword");
-			
+			datasetViewer = prop.getString("datasetViewer");
+			datasetViewerPassword = prop.getString("datasetViewerPassword");
+
 			/**
 			 * A root user on the server that can create databases and set the public schema permissions
 			 */
-			rootDbUser = prop.getProperty("rootDbUser");
-			rootDbPassword = prop.getProperty("rootDbPassword");
-			
+			rootDbUser = prop.getString("rootDbUser");
+			rootDbPassword = prop.getString("rootDbPassword");
+
 			/**
 			 * When ords accesses data via a user command (e.g. the user logs in to a project and adds rows to a table in a 
 			 * database), ords will do that via a special user linked to that of the user logged in. E.g., fred logs in to
 			 * the project and performs the update: under the covers, ords will access the database with user fred_ords. 
 			 * ordsOdbcUserMasterPassword provides the password for this access.
 			 */
-			ordsOdbcUserMasterPassword = prop.getProperty("ordsOdbcUserMasterPassword");
-            if ((ordsOdbcUserMasterPassword == null) || (ordsOdbcUserMasterPassword.length() == 0) ) {
-                log.error("Unable to get odbc master password - defaulting");
-                ordsOdbcUserMasterPassword = ordsPassword;
-            }
-		}
-		catch (IOException e) {
-			log.error("Unable to read main properties file " + CommonVars.mainPropertiesFile, e);
+			ordsOdbcUserMasterPassword = prop.getString("ordsOdbcUserMasterPassword");
+			if ((ordsOdbcUserMasterPassword == null) || (ordsOdbcUserMasterPassword.length() == 0) ) {
+				log.error("Unable to get odbc master password - defaulting");
+				ordsOdbcUserMasterPassword = ordsPassword;
+			}
+		} catch (ConfigurationException e) {
+			log.error("Unable to read main properties file " + dbConfigurationLocation, e);
 		}
 	}
-
-//	public String getOrdsReadOnlyUser() {
-//		return ordsReadOnlyUser;
-//	}
-//
-//	public String getOrdsReadOnlyPassword() {
-//		return ordsReadOnlyPassword;
-//	}
 
 	public String getDatasetViewer() {
 		return datasetViewer;
@@ -83,44 +96,12 @@ public class AuthenticationDetails {
 	public String getRootDbUser() {
 		return rootDbUser;
 	}
-	
+
 	public String getRootDbPassword() {
 		return rootDbPassword;
 	}
-	
+
 	public String getOrdsOdbcUserMasterPassword() {
 		return ordsOdbcUserMasterPassword;
 	}
-	
-	
-	private Properties readProperties(File f) throws IOException{
-		Properties properties = new Properties();
-
-		if (f == null) {
-			throw new IOException("Null properties file");
-		}
-
-		if (!f.exists()) {
-			f = new File(CommonVars.DB_PROPS);
-		}
-
-		properties.load(new FileInputStream(f));
-
-		return properties;
-	}
-	
-    /**
-     * Read the provided properties file.
-     * 
-     * @param props The path of the properties file to read
-     * @return a Properties object containing project properties
-     * @throws IOException if the file is null or cannot be read
-     */
-    private Properties readProperties(String props) throws IOException {
-    	if (props == null) {
-    		throw new IOException("Null properties file");
-    	}
-        return readProperties(new File(props));
-    }
-
 }
