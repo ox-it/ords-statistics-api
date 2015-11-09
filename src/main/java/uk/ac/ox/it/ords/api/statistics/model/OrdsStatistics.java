74cbb78a954de991648daca97b3845a9f6e7c4ba
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-09 11:46:45 +0000

    Added license headers, a bit more JavaDoc, and added suppress some code warnings

--- src/main/java/uk/ac/ox/it/ords/api/statistics/model/OrdsStatistics.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/model/OrdsStatistics.java
@@ -1,17 +1,40 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package uk.ac.ox.it.ords.api.statistics.model;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Reports should specify the following:
+ * Number of current live projects, and number of current live projects that are publicly accessible
+ * Number of current trial projects, and number of current live projects that are publicly accessible
+ * Number of live projects that have been accessed within last 12 months
+ * Number of trial projects that have been accessed within last 12 months
+ * Number of live projects closed over last 12 months
+ * Number of trial projects closed over last 12 months
+ * List of projects, their status (trial or full), their project owners, their last date of access
+ */
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "ordsstatistics")
 public class OrdsStatistics implements Serializable {
--- src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/hibernate/StatisticsServiceImpl.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/hibernate/StatisticsServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.ox.it.ords.api.statistics.services.impl.hibernate;
 
 import java.math.BigInteger;
@@ -11,7 +26,6 @@ import javax.persistence.NonUniqueResultException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Order;
@@ -202,6 +216,7 @@ public class StatisticsServiceImpl implements StatisticsService {
 	}
 
 	
+	@SuppressWarnings("unchecked")
 	private List<String> getDatabases(String server) {
 		List<String> databases = null;
 		Session session = sessionFactory.getCurrentSession();
