fb4278c926a6949810b63ba444155d090b6c16ee
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-09 11:45:46 +0000

    Configure email using mail.properties rather than hardcoded values

--- src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/SendMailTLS.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/services/impl/SendMailTLS.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2015 University of Oxford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package uk.ac.ox.it.ords.api.statistics.services.impl;
 
@@ -14,9 +25,12 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.ox.it.ords.api.statistics.configuration.MetaConfiguration;
 import uk.ac.ox.it.ords.api.statistics.services.MessagingService;
 
 /**
@@ -36,14 +50,36 @@ public class SendMailTLS  implements MessagingService {
     private boolean sendEmails = true;
 
     public SendMailTLS() {
-        log.debug("SendMailTLS init");
+    	
         props = new Properties();
-        props.put("mail.smtp.auth", "false");//true");
-        props.put("mail.smtp.starttls.enable", "false");//true");
-        props.put("mail.smtp.host", "localhost");//smtp.gmail.com");
-        props.put("mail.smtp.port", "25");//587");
-        props.put("mail.smtp.from", "daemons@sysdev.oucs.ox.ac.uk");
+    	
+    	try {
+			PropertiesConfiguration properties = new PropertiesConfiguration(MetaConfiguration.getConfigurationLocation("emailConfiguration"));
+			props.put("mail.smtp.auth", properties.getString("mail.smtp.auth"));
+			props.put("mail.smtp.starttls.enable", properties.getString("mail.smtp.starttls.enable"));
+			props.put("mail.smtp.host", properties.getString("mail.smtp.host"));
+			props.put("mail.smtp.port", properties.getString("mail.smtp.port"));
+			props.put("mail.smtp.from", properties.getString("mail.smtp.from"));
+			props.put("mail.smtp.to", properties.getString("mail.smtp.to"));
+			props.put("mail.smtp.subject", properties.getString("mail.subject"));
+			props.put("mail.smtp.username", properties.getString("mail.username"));
+
+    	} catch (ConfigurationException e) {
+			log.error("Error reading Mail properties file; using hard-coded defaults instead");
+			
+	        props.put("mail.smtp.auth", "false");//true");
+	        props.put("mail.smtp.starttls.enable", "false");//true");
+	        props.put("mail.smtp.host", "localhost");//smtp.gmail.com");
+	        props.put("mail.smtp.port", "25");//587");
+	        props.put("mail.smtp.from", "daemons@sysdev.oucs.ox.ac.uk");
+			props.put("mail.smtp.to", "ords@it.ox.ac.uk");
+			props.put("mail.smtp.subject", "Statistics Information from ORDS");
+			props.put("mail.smtp.username", "ords");
+
+		}
+
         //setupCredentials();
+    	
         this.sendEmails = true;
     }
 
@@ -54,9 +90,9 @@ public class SendMailTLS  implements MessagingService {
     
     private void sendStatsMail(String messageBody) {
     	messageText = messageBody;
-    	email = "ords@it.ox.ac.uk";
-    	subject = "Statistics Information from ORDS";
-    	username = "Mr/Ms Admin";
+    	email = props.getProperty("mail.smtp.to");
+    	subject = props.getProperty("mail.smtp.subject");
+    	username = props.getProperty("mail.smtp.username");
     	sendMail();
     }
 
