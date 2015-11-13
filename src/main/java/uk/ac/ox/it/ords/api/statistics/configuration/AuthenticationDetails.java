85ba0d01caa15656404451e9273a3a3291ef750e
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-13 21:19:14 +0000

    Load defaults using MetaConfiguration

--- src/main/java/uk/ac/ox/it/ords/api/statistics/configuration/AuthenticationDetails.java
+++ src/main/java/uk/ac/ox/it/ords/api/statistics/configuration/AuthenticationDetails.java
@@ -15,8 +15,6 @@
  */
 package uk.ac.ox.it.ords.api.statistics.configuration;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,51 +31,38 @@ public class AuthenticationDetails {
 
 	public AuthenticationDetails() {
 
-		String dbConfigurationLocation = DEFAULT_DATABASE_CONFIG_LOCATION;
-		dbConfigurationLocation = MetaConfiguration.getConfigurationLocation("databasePropertiesLocation");
-		if (dbConfigurationLocation == null){
-			log.warn("No server configuration location set; using defaults");
-			dbConfigurationLocation = DEFAULT_DATABASE_CONFIG_LOCATION;
-		}
+		//
+		// Load the Database Properties
+		//
 
-		try {
+		/**
+		 * The user that can connect to and read/write from/to the ords database. Used for hibernate.
+		 */
+		ordsUser = MetaConfiguration.getConfiguration().getString("user");
+		ordsPassword = MetaConfiguration.getConfiguration().getString("password");
 
-			//
-			// Load the Database Properties
-			//
-			PropertiesConfiguration prop = new PropertiesConfiguration(dbConfigurationLocation);
+		/**
+		 * A read only user used to view public datasets
+		 */
+		datasetViewer = MetaConfiguration.getConfiguration().getString("datasetViewer");
+		datasetViewerPassword = MetaConfiguration.getConfiguration().getString("datasetViewerPassword");
 
-			/**
-			 * The user that can connect to and read/write from/to the ords database. Used for hibernate.
-			 */
-			ordsUser = prop.getString("user");
-			ordsPassword = prop.getString("password");
+		/**
+		 * A root user on the server that can create databases and set the public schema permissions
+		 */
+		rootDbUser = MetaConfiguration.getConfiguration().getString("rootDbUser");
+		rootDbPassword = MetaConfiguration.getConfiguration().getString("rootDbPassword");
 
-			/**
-			 * A read only user used to view public datasets
-			 */
-			datasetViewer = prop.getString("datasetViewer");
-			datasetViewerPassword = prop.getString("datasetViewerPassword");
-
-			/**
-			 * A root user on the server that can create databases and set the public schema permissions
-			 */
-			rootDbUser = prop.getString("rootDbUser");
-			rootDbPassword = prop.getString("rootDbPassword");
-
-			/**
-			 * When ords accesses data via a user command (e.g. the user logs in to a project and adds rows to a table in a 
-			 * database), ords will do that via a special user linked to that of the user logged in. E.g., fred logs in to
-			 * the project and performs the update: under the covers, ords will access the database with user fred_ords. 
-			 * ordsOdbcUserMasterPassword provides the password for this access.
-			 */
-			ordsOdbcUserMasterPassword = prop.getString("ordsOdbcUserMasterPassword");
-			if ((ordsOdbcUserMasterPassword == null) || (ordsOdbcUserMasterPassword.length() == 0) ) {
-				log.error("Unable to get odbc master password - defaulting");
-				ordsOdbcUserMasterPassword = ordsPassword;
-			}
-		} catch (ConfigurationException e) {
-			log.error("Unable to read main properties file " + dbConfigurationLocation, e);
+		/**
+		 * When ords accesses data via a user command (e.g. the user logs in to a project and adds rows to a table in a 
+		 * database), ords will do that via a special user linked to that of the user logged in. E.g., fred logs in to
+		 * the project and performs the update: under the covers, ords will access the database with user fred_ords. 
+		 * ordsOdbcUserMasterPassword provides the password for this access.
+		 */
+		ordsOdbcUserMasterPassword = MetaConfiguration.getConfiguration().getString("ordsOdbcUserMasterPassword");
+		if ((ordsOdbcUserMasterPassword == null) || (ordsOdbcUserMasterPassword.length() == 0) ) {
+			log.error("Unable to get odbc master password - defaulting");
+			ordsOdbcUserMasterPassword = ordsPassword;
 		}
 	}
 
